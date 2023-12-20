d4a963fd68da7aaac0b9448d048fe0cf17193e00
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2023-12-20 17:34:31 +0100

    norminette: OK

--- so_long_mac/includes/so_long.h
+++ so_long_mac/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: bautrodr <bautrodr@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 20:07:34 by bautrodr          #+#    #+#             */
-/*   Updated: 2023/12/20 14:16:05 by bautrodr         ###   ########.fr       */
+/*   Updated: 2023/12/20 17:34:19 by bautrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,33 @@
 # define UP 3
 # define DOWN 4
 
-//# include "../mlx/mlx_linux/mlx.h"
-# include "../mlx/mlx_mac/mlx.h"
-# include "../libft/libft.h"
 # include "../get_next_line/get_next_line.h"
-# include<unistd.h>
-# include<stdlib.h>
-# include<limits.h>
-# include<stdio.h>
-# include<fcntl.h>
+# include "../libft/libft.h"
+# include "../mlx/mlx_mac/mlx.h"
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_textures
 {
-	void	*floor;
-	void	*coin;
-	void	*closed_door;
-	void	*opened_door;
-	void	*wall;
-}			t_textures;
+	void		*floor;
+	void		*coin;
+	void		*closed_door;
+	void		*opened_door;
+	void		*wall;
+}				t_textures;
 
 typedef struct s_player
 {
-	void	*player_up;
-	void	*player_down;
-	void	*player_left;
-	void	*player_right;
-}			t_player;
+	void		*player_up;
+	void		*player_down;
+	void		*player_left;
+	void		*player_right;
+}				t_player;
 
-typedef struct	s_game
+typedef struct s_game
 {
 	void		*window;
 	void		*mlx;
@@ -69,35 +68,24 @@ typedef struct	s_game
 	t_player	player;
 }				t_game;
 
-
-//free
-int		destroy_program(t_game *game);
-void	free_map(t_game *game);
-void	free_visited(int **visited, t_game *game);
-//graphics
-void	init_img(t_game *game);
-void	add_graphics(t_game *game);
-void	print_map(char *line, t_game *game, int index);
-
-//keys
-int		key_hook(int keycode, t_game *game);
-
-//print_errors
-void	print_error(t_game *game, int n);
-//player
-void	print_player(t_game *game, int width, int heigth);
-void	update_player_pos(t_game *game, int key);
-
-//check path
-void	flood_fill(t_game *game, int row, int col, int **visited);
-int		valid_path(t_game *game, int i, int p_row, int p_col);
-// map
-int		init_map(char *map, t_game *game, int linecount);
-int		check_map(t_game *game, char *map);
-int		more_checks(t_game *game, char *map);
-int		check_filetype(char *str);
-void	check_repeated(t_game *game, int i, int j);
-void	print_map(char *line, t_game *game, int index);
-int		same_length(t_game *game);
+int				destroy_program(t_game *game);
+void			free_map(t_game *game);
+void			free_visited(int **visited, t_game *game);
+void			init_img(t_game *game);
+void			add_graphics(t_game *game);
+void			print_map(char *line, t_game *game, int index);
+int				key_hook(int keycode, t_game *game);
+void			print_error(t_game *game, int n);
+void			print_player(t_game *game, int width, int heigth);
+void			update_player_pos(t_game *game, int key);
+void			flood_fill(t_game *game, int row, int col, int **visited);
+int				valid_path(t_game *game, int i, int p_row, int p_col);
+int				init_map(char *map, t_game *game, int linecount);
+int				check_map(t_game *game, char *map);
+int				more_checks(t_game *game, char *map);
+int				check_filetype(char *str);
+void			check_repeated(t_game *game, int i, int j);
+void			print_map(char *line, t_game *game, int index);
+int				same_length(t_game *game);
 
 #endif
